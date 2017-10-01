855b12a26730abc456c888f783e0218e02db99dc
jeffskinnerbox <jeff.irland@verizon.net>
2017-10-01 12:36:27 -0400

    added a with / without use of Numpy's dstack

--- test/videostream_test_3.py
+++ test/videostream_test_3.py
@@ -11,7 +11,6 @@
 #   https://www.pyimagesearch.com/2017/02/06/faster-video-file-fps-with-cv2-videocapture-and-opencv/
 
 
-
 # import the necessary packages
 from imutils.video import FileVideoStream
 from imutils.video import FPS
@@ -27,7 +26,10 @@ ap.add_argument("-v", "--video", required=True,
                 help="path to input video file")
 args = vars(ap.parse_args())
 
+# ############################ With Numpy's dstack #############################
+
 # start the file video stream thread and allow the buffer to start to fill
+print("Using Numpy's dstack")
 print("Starting video file thread...")
 fvs = FileVideoStream(args["video"]).start()
 time.sleep(1.0)
@@ -37,8 +39,8 @@ fps = FPS().start()
 
 # loop over frames from the video file stream
 while fvs.more():
-    # grab the frame from the threaded video file stream, resize
-    # it, and convert it to grayscale (while still retaining 3 channels)
+    # grab the frame from the threaded video file stream, resize it,
+    # and convert it to grayscale (while still retaining 3 channels)
     frame = fvs.read()
     frame = imutils.resize(frame, width=450)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -47,7 +49,54 @@ while fvs.more():
     # display the size of the queue on the frame
     cv2.putText(frame, "Queue Size: {}".format(fvs.Q.qsize()), (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-    # show the frame and update the FPS counter
+
+    # show a screen image of the frame
+    cv2.imshow("Frame", frame)
+
+    # update the frame count
+    fps.update()
+
+    # if the `q` or esc key was pressed, break from the loop
+    key = cv2.waitKey(1)
+    if chr(key & 255) == 'q' or key == 27:
+        fps.stop()
+        print("File stopped by user ...")
+        break
+
+# stop the timer and display FPS information
+fps.stop()
+print("\telasped time: {:.2f}".format(fps.elapsed()))
+print("\tapprox. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+fvs.stop()
+
+# ########################## Without Numpy's dstack ############################
+
+# start the file video stream thread and allow the buffer to start to fill
+print("\nNOT using Numpy's dstack")
+print("Starting video file thread...")
+fvs = FileVideoStream(args["video"]).start()
+time.sleep(1.0)
+
+# start the FPS timer
+fps = FPS().start()
+
+# loop over frames from the video file stream
+while fvs.more():
+    # grab the frame from the threaded video file stream, resize it,
+    # and convert it to grayscale (while still retaining 3 channels)
+    frame = fvs.read()
+    frame = imutils.resize(frame, width=450)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#    frame = np.dstack([frame, frame, frame])
+
+    # display the size of the queue on the frame
+    cv2.putText(frame, "Queue Size: {}".format(fvs.Q.qsize()), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    # show a screen image of the frame
     cv2.imshow("Frame", frame)
 
     # update the frame count
