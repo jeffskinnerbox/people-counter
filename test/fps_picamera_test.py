c22398b49413ac5466c4b9ad97f05e942b3ac187
jeffskinnerbox <jeff.irland@verizon.net>
2017-10-01 13:21:00 -0400

    renamed the files

--- test/fps_picamera_test.py
+++ test/fps_picamera_test.py
@@ -4,23 +4,24 @@
 # Version:      0.1.0
 #
 # USAGE
-#    python webcamera_fps_test.py  -  report the frames per second
-#    python webcamera_fps_test.py -d  -  also display the video in a window
+#    python fps_picamera_test.py  -  just report the mesurements
+#    python fps_picamera_test.py -d  -  also display the video in a window
 #
 # SOURCE
 #    "Increasing Raspberry Pi FPS with Python and OpenCV"
-#    https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+#    https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/           #noqa
 
 
 # import the necessary packages
 from __future__ import print_function
-from imutils.video import WebcamVideoStream
+from imutils.video.pivideostream import PiVideoStream
 from imutils.video import FPS
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import argparse
 import imutils
 import time
 import cv2
-import sys
 
 
 # construct the argument parse and parse the arguments
@@ -35,34 +36,43 @@ args = vars(ap.parse_args())
 
 # ################################## 1st Pass ##################################
 
-# grab a pointer to the video stream and initialize the FPS counter
+# initialize the camera and stream
+camera = PiCamera()
+camera.resolution = (320, 240)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(320, 240))
+stream = camera.capture_continuous(rawCapture,
+                                   format="bgr", use_video_port=True)
+
+# allow the camera to warmup and start the FPS counter
 print("1st Pass: Reading", args["num_frames"], "frames from web camera.")
-print("Web Camera warming up ...")
-stream = cv2.VideoCapture(0)
+print("Pi Camera warming up ...")
 time.sleep(2.0)
 fps = FPS().start()
 
 # loop over some frames
-while fps._numFrames < args["num_frames"]:
+for (i, f) in enumerate(stream):
     # grab the frame from the stream and resize it to have a maximum
     # width of 400 pixels
-    (grabbed, frame) = stream.read()
-    if frame == None:
-        print("USB Web Camera has no frame ... exiting")
-        sys.exit()
-    else:
-        frame = imutils.resize(frame, width=400)
+    frame = f.array
+    frame = imutils.resize(frame, width=400)
 
     # check to see if the frame should be displayed to our screen
     if args["display"]:
         cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1)
+        key = cv2.waitKey(1) & 0xFF
         if chr(key & 255) == 'q' or key == 27:
             print("Camera stopped by user ...")
             break
 
-    # update the FPS counter
+    # clear the stream in preparation for the next frame and update
+    # the FPS counter
+    rawCapture.truncate(0)
     fps.update()
+
+    # check to see if the desired number of frames have been reached
+    if i == args["num_frames"]:
+        break
 
 # stop the timer and display FPS information
 fps.stop()
@@ -70,18 +80,19 @@ print("\telasped time: {:.2f}".format(fps.elapsed()))
 print("\tapprox. FPS: {:.2f}".format(fps.fps()))
 
 # do a bit of cleanup
-stream.release()
 cv2.destroyAllWindows()
-
+rawCapture.close()
+camera.close()
 
 
 # ########################## 2nd Pass With Threading ###########################
 
-# created a *threaded *video stream, allow the camera senor to warmup,
+# created a *threaded *video stream, allow the camera sensor to warmup,
 # and start the FPS counter
 print("\n2nd Pass: Reading", args["num_frames"], "frames from web camera.")
-print("Using THREADED frames from webcam...")
-vs = WebcamVideoStream(src=0).start()
+print("Using THREADED frames from Pi Camera...")
+vs = PiVideoStream().start()
+time.sleep(2.0)
 fps = FPS().start()
 
 # loop over some frames...this time using the threaded stream
@@ -89,16 +100,12 @@ while fps._numFrames < args["num_frames"]:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 400 pixels
     frame = vs.read()
-    if frame == None:
-        print("USB Web Camera has no frame ... exiting")
-        sys.exit()
-    else:
-        frame = imutils.resize(frame, width=400)
+    frame = imutils.resize(frame, width=400)
 
     # check to see if the frame should be displayed to our screen
     if args["display"]:
         cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1)
+        key = cv2.waitKey(1) & 0xFF
         if chr(key & 255) == 'q' or key == 27:
             print("Camera stopped by user ...")
             break
--- test/fps_webcamera_test.py
+++ test/fps_webcamera_test.py
@@ -4,24 +4,23 @@
 # Version:      0.1.0
 #
 # USAGE
-#    python picamera_fps_test.py  -  just report the mesurements
-#    python picamera_fps_test.py -d  -  also display the video in a window
+#    python fps_webcamera_test.py  -  report the frames per second
+#    python fps_webcamera_test.py -d  -  also display the video in a window
 #
 # SOURCE
 #    "Increasing Raspberry Pi FPS with Python and OpenCV"
-#    https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/# USAGE
+#    https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/        #noqa
 
 
 # import the necessary packages
 from __future__ import print_function
-from imutils.video.pivideostream import PiVideoStream
+from imutils.video import WebcamVideoStream
 from imutils.video import FPS
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import argparse
 import imutils
 import time
 import cv2
+import sys
 
 
 # construct the argument parse and parse the arguments
@@ -36,43 +35,34 @@ args = vars(ap.parse_args())
 
 # ################################## 1st Pass ##################################
 
-# initialize the camera and stream
-camera = PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(320, 240))
-stream = camera.capture_continuous(rawCapture,
-                                   format="bgr", use_video_port=True)
-
-# allow the camera to warmup and start the FPS counter
+# grab a pointer to the video stream and initialize the FPS counter
 print("1st Pass: Reading", args["num_frames"], "frames from web camera.")
-print("Pi Camera warming up ...")
+print("Web Camera warming up ...")
+stream = cv2.VideoCapture(0)
 time.sleep(2.0)
 fps = FPS().start()
 
 # loop over some frames
-for (i, f) in enumerate(stream):
+while fps._numFrames < args["num_frames"]:
     # grab the frame from the stream and resize it to have a maximum
     # width of 400 pixels
-    frame = f.array
-    frame = imutils.resize(frame, width=400)
+    (grabbed, frame) = stream.read()
+    if frame is None:
+        print("USB Web Camera has no frame ... exiting")
+        sys.exit()
+    else:
+        frame = imutils.resize(frame, width=400)
 
     # check to see if the frame should be displayed to our screen
     if args["display"]:
         cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+        key = cv2.waitKey(1)
         if chr(key & 255) == 'q' or key == 27:
             print("Camera stopped by user ...")
             break
 
-    # clear the stream in preparation for the next frame and update
-    # the FPS counter
-    rawCapture.truncate(0)
+    # update the FPS counter
     fps.update()
-
-    # check to see if the desired number of frames have been reached
-    if i == args["num_frames"]:
-        break
 
 # stop the timer and display FPS information
 fps.stop()
@@ -80,20 +70,17 @@ print("\telasped time: {:.2f}".format(fps.elapsed()))
 print("\tapprox. FPS: {:.2f}".format(fps.fps()))
 
 # do a bit of cleanup
+stream.release()
 cv2.destroyAllWindows()
-rawCapture.close()
-camera.close()
-
 
 
 # ########################## 2nd Pass With Threading ###########################
 
-# created a *threaded *video stream, allow the camera sensor to warmup,
+# created a *threaded *video stream, allow the camera senor to warmup,
 # and start the FPS counter
 print("\n2nd Pass: Reading", args["num_frames"], "frames from web camera.")
-print("Using THREADED frames from Pi Camera...")
-vs = PiVideoStream().start()
-time.sleep(2.0)
+print("Using THREADED frames from webcam...")
+vs = WebcamVideoStream(src=0).start()
 fps = FPS().start()
 
 # loop over some frames...this time using the threaded stream
@@ -101,12 +88,16 @@ while fps._numFrames < args["num_frames"]:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 400 pixels
     frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+    if frame is None:
+        print("USB Web Camera has no frame ... exiting")
+        sys.exit()
+    else:
+        frame = imutils.resize(frame, width=400)
 
     # check to see if the frame should be displayed to our screen
     if args["display"]:
         cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+        key = cv2.waitKey(1)
         if chr(key & 255) == 'q' or key == 27:
             print("Camera stopped by user ...")
             break
