1c1101d7c81795bfabedb12566a9df52ef0f4ee1
Jeff Irland <jeffskinnerbox@yahoo.com>
2017-10-01 22:39:27 -0400

    got pi camera working but with hard coding for  get() parameters function

--- vstream.py
+++ vstream.py
@@ -3,6 +3,7 @@
 # Version:      0.2.0
 
 # import the necessary packages
+import cv2
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 
@@ -11,7 +12,7 @@ class VStream:
     vsource = None
 
     def __init__(self, source, path=None, queueSize=128, src=0,
-                 resolution=(320, 240), framerate=32):
+                 resolution=(640, 480), framerate=30):
         # initialize the video stream along with the boolean
         # used to indicate if the thread should be stopped or not
         self.vsource = source
@@ -51,7 +52,39 @@ class VStream:
 
     def get(self, obj):
         # acess cv2.VideoCapture.get() within the FileVideoStream class
-        if self.vsource == 'file':
-            return self.stream.stream.get(obj)
+        if self.vsource == 'picamera':
+            if obj == cv2.CAP_PROP_FRAME_WIDTH:
+                return 640
+            elif obj == cv2.CAP_PROP_FRAME_HEIGHT:
+                return 480
+            elif obj == cv2.CAP_PROP_FPS:
+                return 30
+            elif obj == cv2.CAP_PROP_FRAME_COUNT:
+                return 1
         else:
             return self.stream.stream.get(obj)
+
+"""
+Picamera 1.13 Documentation (Release 1.13) - https://media.readthedocs.org/pdf/picamera/latest/picamera.pdf
+
+        self.POS_MSEC = vidcap.get(CAP_PROP_POS_MSEC)             # Current position of the video file in milliseconds or video capture timestamp
+        self.POS_FRAMES = vidcap.get(CAP_PROP_POS_FRAMES)         # 0-based index of the frame to be decoded/captured next
+        self.POS_AVI_RATIO = vidcap.get(CAP_PROP_POS_AVI_RATIO)   # Relative position of the video file: 0 - start of the film, 1 - end of the film
+        self.FRAME_WIDTH = vidcap.get(CAP_PROP_FRAME_WIDTH)       # Width of the frames in the video stream
+        self.FRAME_HEIGHT = vidcap.get(CAP_PROP_FRAME_HEIGHT)     # Height of the frames in the video stream
+        self.FPS = vidcap.get(CAP_PROP_FPS)                       # Frame rate
+        self.FOURCC = vidcap.get(CAP_PROP_FOURCC)                 # 4-character code of codec
+        self.FRAME_COUNT = vidcap.get(CAP_PROP_FRAME_COUNT)       # Number of frames in the video file
+        self.FORMAT = vidcap.get(CAP_PROP_FORMAT)                 # Format of the Mat objects returned by retrieve()
+        self.MODE = vidcap.get(CAP_PROP_MODE)                     # Backend-specific value indicating the current capture mode
+        self.BRIGHTNESS = vidcap.get(CAP_PROP_BRIGHTNESS)         # Brightness of the image (only for cameras)
+        self.CONTRAST = vidcap.get(CAP_PROP_CONTRAST)             # Contrast of the image (only for cameras)
+        self.SATURATION = vidcap.get(CAP_PROP_SATURATION)         # Saturation of the image (only for cameras)
+        self.HUE = vidcap.get(CAP_PROP_HUE)                       # Hue of the image (only for cameras)
+        self.GAIN = vidcap.get(CAP_PROP_GAIN)                     # Gain of the image (only for cameras)
+        self.EXPOSURE = vidcap.get(CAP_PROP_EXPOSURE)             # Exposure (only for cameras)
+        self.CONVERT_RGB = vidcap.get(CAP_PROP_CONVERT_RGB)       # Boolean flags indicating whether images should be converted to RGB
+        self.WHITE_BALANCE = vidcap.get(CAP_PROP_WHITE_BALANCE)   # Currently not supported
+        self.RECTIFICATION = vidcap.get(CAP_PROP_RECTIFICATION)   # Rectification flag for stereo cameras (note: only supported by DC1394 v 2.x backend currently)
+
+"""
