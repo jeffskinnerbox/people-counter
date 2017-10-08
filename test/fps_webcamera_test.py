410be571f88355a535597ec03d5e1ef07866d146
jeffskinnerbox <jeff.irland@verizon.net>
2017-10-08 11:45:58 -0400

    fast but seems strange to see such a speed up

--- test/fps_webcamera_test.py
+++ test/fps_webcamera_test.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 #
 # Maintainer:   jeffskinnerbox@yahoo.com / www.jeffskinnerbox.me
-# Version:      0.1.0
+# Version:      0.3.0
 #
 # USAGE
 #    python fps_webcamera_test.py  -  report the frames per second
 #    python fps_webcamera_test.py -d  -  also display the video in a window
 #
 # SOURCE
-#    "Increasing Raspberry Pi FPS with Python and OpenCV"
-#    https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/        #noqa
+#    "Increasing webcam FPS with Python and OpenCV"
+#    https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/        #noqa
+#
+# PURPOSE
+#    This script compare the runtime and frame rate of processing a usb / web
+#    camera when using Adrian Rosebrock's imutils process threading utilities.
 
 
 # import the necessary packages
@@ -20,7 +24,6 @@ import argparse
 import imutils
 import time
 import cv2
-import sys
 
 
 # construct the argument parse and parse the arguments
@@ -29,8 +32,10 @@ ap.add_argument("-n", "--num-frames",
                 help="# of frames to loop over for FPS test",
                 type=int, default=100)
 ap.add_argument("-d", "--display",
-                help="Whether or not frames should be displayed",
-                action='store_true')
+                help="display the video as its processed",
+                required=False,
+                action='store_true',
+                default=False)
 args = vars(ap.parse_args())
 
 # ################################## 1st Pass ##################################
@@ -49,7 +54,7 @@ while fps._numFrames < args["num_frames"]:
     (grabbed, frame) = stream.read()
     if frame is None:
         print("USB Web Camera has no frame ... exiting")
-        sys.exit()
+        break
     else:
         frame = imutils.resize(frame, width=400)
 
@@ -79,8 +84,10 @@ cv2.destroyAllWindows()
 # created a *threaded *video stream, allow the camera senor to warmup,
 # and start the FPS counter
 print("\n2nd Pass: Reading", args["num_frames"], "frames from web camera.")
-print("Using THREADED frames from webcam...")
+print("Using THREADED frames from webcam")
+print("Web Camera warming up ...")
 vs = WebcamVideoStream(src=0).start()
+time.sleep(2.0)
 fps = FPS().start()
 
 # loop over some frames...this time using the threaded stream
@@ -90,7 +97,7 @@ while fps._numFrames < args["num_frames"]:
     frame = vs.read()
     if frame is None:
         print("USB Web Camera has no frame ... exiting")
-        sys.exit()
+        break
     else:
         frame = imutils.resize(frame, width=400)
 
@@ -109,7 +116,86 @@ while fps._numFrames < args["num_frames"]:
 fps.stop()
 print("\telasped time: {:.2f}".format(fps.elapsed()))
 print("\tapprox. FPS: {:.2f}".format(fps.fps()))
+print("Appears the same frame is being repeatedly read??")
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+
+
+"""
+# import the necessary packages
+from __future__ import print_function
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+    help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+    help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
+# grab a pointer to the video stream and initialize the FPS counter
+print("[INFO] sampling frames from webcam...")
+stream = cv2.VideoCapture(0)
+fps = FPS().start()
+
+# loop over some frames
+while fps._numFrames < args["num_frames"]:
+    # grab the frame from the stream and resize it to have a maximum
+    # width of 400 pixels
+    (grabbed, frame) = stream.read()
+    frame = imutils.resize(frame, width=400)
+
+    # check to see if the frame should be displayed to our screen
+    if args["display"] > 0:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+    # update the FPS counter
+    fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+stream.release()
+cv2.destroyAllWindows()
+
+# created a *threaded* video stream, allow the camera sensor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from webcam...")
+vs = WebcamVideoStream(src=0).start()
+fps = FPS().start()
+
+# loop over some frames...this time using the threaded stream
+while fps._numFrames < args["num_frames"]:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    # check to see if the frame should be displayed to our screen
+    if args["display"] > 0:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+    # update the FPS counter
+    fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
+"""
