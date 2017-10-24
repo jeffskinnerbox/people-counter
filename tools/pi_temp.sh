f3db642fa3740a0836d446b13c1b7a719f9b6736
Jeff Irland <jeffskinnerbox@yahoo.com>
2017-10-23 23:09:07 -0400

    minor changes to output format

--- tools/pi_temp.sh
+++ tools/pi_temp.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-# Script: my-pi-temp.sh
-# Purpose: Display the ARM CPU and GPU  temperature of Raspberry Pi 2/3 
-# Author: Vivek Gite <www.cyberciti.biz> under GPL v2.x+
-# -------------------------------------------------------
+#
+# Maintainer:   jeffskinnerbox@yahoo.com / www.jeffskinnerbox.me
+# Version:      0.4.1
+#
+# Purpose: Display the ARM CPU and GPU temperature of Raspberry Pi 2/3 
+
 
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
 while true; do
-    echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
-    echo "CPU => $((cpu/1000))'C"
+    echo "GPU: $(/opt/vc/bin/vcgencmd measure_temp)"
+    echo "CPU: temp=$(($(</sys/class/thermal/thermal_zone0/temp) / 1000))'C"
     sleep 2
 done
+
+
